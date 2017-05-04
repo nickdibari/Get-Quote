f6d2c8381af693e9f9cddad56316e672e232ce04
nickdibari <ndibari@fordham.edu>
2017-05-04 08:29:05 -0400

    PEP8 getquote

--- getquote.py
+++ getquote.py
@@ -1,24 +1,26 @@
 #! /usr/bin/python3
 
-#* GetQuote.py                                   *#
-#* Nicholas DiBari                               *#
-#* --------------------------------------------- *#
-#* Prints quote from brainyquote.com from author *# 
-#* that user provides                            *#
-#* --------------------------------------------- *#
+# * GetQuote.py                                   * #
+# * Nicholas DiBari                               * #
+# * --------------------------------------------- * #
+# * Prints quote from brainyquote.com from author * #
+# * that user provides                            * #
+# * --------------------------------------------- * #
 
-import sys # Get argument
-import requests # Get HTML
-import bs4 # Parsing HTML
-import shelve # Database Management
-import argparse # Command line argument parsing
-from datetime import datetime # Date/Time Information
+import sys  # Get argument
+import requests  # Get HTML
+import bs4  # Parsing HTML
+import shelve  # Database Management
+import argparse  # Command line argument parsing
+from datetime import datetime  # Date/Time Information
 
-#1. Arg Parser Definition
+
 def ArgParser():
+    description = 'The Easy to use Quote Search'
+
     parser = argparse.ArgumentParser(prog='./getquote.py',
                                      usage='%(prog)s AUTHOR [options]',
-                                     description='The easy to use Quote Finder')
+                                     description=description)
 
     parser.add_argument('author', metavar='AUTHOR', nargs='*',
                         help='Search for quotes from AUTHOR')
@@ -33,35 +35,35 @@ def ArgParser():
 
     return parser
 
-#2. Get Quote
+
 def GetQuote(author):
     results = []
 
     print('Searching for {}...'.format(author))
 
-    #Open website with requests 
-    HTML = requests.get('http://brainyquote.com/search_results.html?q=' + author )
-    HTML.raise_for_status() #Check to ensure page was downloaded correctly
+    # Open website with requests
+    response = requests.get('http://brainyquote.com/search_results.html?q={}'
+                            .format(author))
+    response.raise_for_status()  # Check to ensure page was downloaded OK
 
-    #Create Beautiful Soup object to parse
-    QuoteObject = bs4.BeautifulSoup(HTML.text, "html.parser")
+    # Create Beautiful Soup object to parse
+    QuoteObject = bs4.BeautifulSoup(response.text, "html.parser")
 
-    #Parse Beautiful Soup object for quote
-    quotes = QuoteObject.select('#quotesList a') #Returns the element <a> located within class 'quotesList'
+    # Parse Beautiful Soup object for quote
+    quotes = QuoteObject.select('#quotesList a')
 
     for quote in quotes:
-      if quote.get('title') == 'view quote':
-          results.append(quote)
+        if quote.get('title') == 'view quote':
+            results.append(quote)
 
     return results
 
-#3. Print Quote
-def PrintQuotes(quotes, author, numQuotes):
 
+def PrintQuotes(quotes, author, numQuotes):
     if numQuotes > len(quotes):
         numQuotes = len(quotes)
 
-    #Print all quotes retrievd
+    # Print all quotes retrievd
     print(" ")
     print("Found the following matches for " + author + ":")
     print("-----------------------------------------------")
@@ -69,42 +71,45 @@ def PrintQuotes(quotes, author, numQuotes):
         print(str(i) + ". " + quotes[i].getText())
         print(' ')
 
-#4. Save Quote
-def SaveQuote(quotes_DB, quotes, author):  
-    
+
+def SaveQuote(quotes_DB, quotes, author):
     while True:
-        choice=input("Please pick a quote to save (or enter done to exit): ")
-        
-        #CASE 1 [EXIT]. Check if user wishes to exit program
-        if choice == "done":
+        choice = input('Please pick a quote to save (or enter done to exit): ')
+
+        # CASE 1 [EXIT]. Check if user wishes to exit program
+        if choice == 'done':
             break
-        
+
         else:
             try:
                 choice = int(choice)
 
-                #CASE 2 [INPUT ERROR]. Check that number is in rangee
-                if choice<0 or choice>len(quotes):
-                    print("That is an invalid input. Please enter a number between 0 and " + len(quotes))
+                # CASE 2 [INPUT ERROR]. Check that number is in range
+                if choice < 0 or choice > len(quotes):
+                    print('That is an invalid input.\
+                          Please enter a number between 0 and {}'
+                          .format(len(quotes)))
 
-                #CASE 3 [BASE CASE]. Add selected quote to database
+                # CASE 3 [BASE CASE]. Add selected quote to database
                 else:
-                    DateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Get current date/time
-                    Author_to_Add = str(author) + " " + DateTime # Get name of author for quote
-                    Quote_to_Add = str(quotes[int(choice)].getText()) # Get quote to add
+                    DateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    Author_to_Add = '{0} {1}'.format(author, DateTime)
+                    Quote_to_Add = str(quotes[choice].getText())
                     quotes_DB[Author_to_Add] = Quote_to_Add
-                    print("Saved the quote you picked by " + author + ". Good choice")
+
+                    print('Saved the quote you picked by {}. Good choice'
+                          .format(author))
 
             except ValueError:
                 print('Enter in a number silly!')
-    
-#Main Function
+
+
 def Main():
-    quotes_DB=shelve.open('.Quotes.db')
+    quotes_DB = shelve.open('.Quotes.db')
 
     parser = ArgParser()
     args = parser.parse_args(sys.argv[1:])
-    
+
     author = ' '.join(args.author)
     numQuotes = args.numQuotes
 
@@ -121,7 +126,8 @@ def Main():
 
         print("Goodbye!")
 
-    quotes_DB.close() # Close Database
+    quotes_DB.close()  # Close Database
+
 
 if __name__ == '__main__':
     Main()
